f4c90f869bcb8074a46f8975c023e921b7ca4664
jmjcodez <johnmjones94@gmail.com>
2024-12-27 17:26:07 -0500

    Add function to play single round Rock Paper Scissors
    Updated code to allow case insensitive input, functional code
    for single game

--- script.js
+++ script.js
@@ -4,13 +4,13 @@
 let randomNum = Math.floor(Math.random()*3);
 function getComputerChoice() {
 	if (randomNum === 0){
-		return "Rock";
+		return "rock";
 	}
 	else if (randomNum === 1){
-		return "Paper"
+		return "paper"
 	}
 	else {
-		return "Scissors"
+		return "scissors"
 	}
 }
 
@@ -19,21 +19,44 @@ console.log(computerChoice);
 
 /* Write logic to get the humans choice. Make  function for this algorithm. Create a prompt for user input text, limit it so that can only select Rock Paper or Scissors. Present an error message until the correct choice is picked. Only accept those 3. Store that value into a variable for humanChoice */
 
+/* Declare global variables for storing the scores of Human and Computer, set initial value */
 let userChoice;
+let humanScore = 0;
+let computerScore = 0;
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
 
 function getHumanChoice() {
 	let keepGoing = true;
 	while(keepGoing){
 		userChoice = prompt("Choose: Rock, Paper, or Scissors?","");
+		let processedName = userChoice.toLowerCase();
+		console.log(processedName);
 
-		if (userChoice === "Rock" || userChoice === "Paper" || userChoice === "Scissors"){
-			keepGoing = "false";
-			return userChoice;
+		if (processedName === "rock" || processedName === "paper" || processedName === "scissors"){
+			keepGoing = false;
+			return processedName;
 		}
 		else{
 			alert("Invalid input, try again!")
 		}
 	}
 	}
-let humanChoice = getHumanChoice();
-console.log(humanChoice);
+
+/* Write logic to play a single round, create function that takes human and computer choices as arguments. Plays a round and increments the winner's score and logs a winner. Modify the user input for human choice to take case insensitive values. Create hiearachy of all scenarios to determine winner of points and ties.  */
+
+function playRound (humanChoice, computerChoice){
+	if (humanChoice === computerChoice){
+		console.log("Tie Game!")
+	}
+	else if ((humanChoice === "rock" && computerChoice === "scissors")|| humanChoice === "scissors" && computerChoice === "paper"){
+		humanScore++;
+		console.log("Human Wins!");
+	}
+	else {
+		computerScore++;
+		console.log("Computer Wins!");
+	}
+	}
+
